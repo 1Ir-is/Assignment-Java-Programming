64af0b2612a9509d34512f24d2b99b1b0fc3e432
IriS <mhuynk1005@gmail.com>
2022-08-22 15:45:10 +0700

    Add function

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -1,17 +1,41 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class StudentManagement extends JFrame {
+    public JTextField nameField;
+    public JTextField idField;
+    public JTextField gradeField;
+    public JRadioButton maleRadioButton;
+    public JRadioButton femaleRadioButton;
+    public JRadioButton otherRadioButton;
 
     ArrayList<Student> studentArrayList = new ArrayList<>();
     static String databaseFile = "StudentManagement.txt";
-
-
 
     public static void main(String[] args) throws IOException {
         StudentManagement studentManagement = new StudentManagement();
         studentManagement.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         studentManagement.setVisible(true);
+    }
+
+    // ADD FUNCTION.
+    public void addStudent(ActionEvent e){
+        String name = nameField.getText().trim();
+        String id = idField.getText().trim();
+        String gender;
+        if (maleRadioButton.isSelected()){
+            gender = maleRadioButton.getText();
+        } else if (femaleRadioButton.isSelected()) {
+            gender = femaleRadioButton.getText();
+        } else if (otherRadioButton.isSelected()) {
+            gender = otherRadioButton.getText();
+        }
+        else {
+            gender = "";
+        }
+        String grade = gradeField.getText().toUpperCase();
+
     }
 }
